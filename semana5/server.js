85fdebbf5cac8505cbb8d637148e42355ddde1f2
dianalizarazorincon-bit <7.10dianalizarazo@gmail.com>
2026-05-20 18:33:46 -0500

    Initialize semana5 directory and server files
    Initial setup for semana5 with server and related files.

---
+++ semana5/server.js
@@ -0,0 +1,29 @@
+pwd
+ls
+mkdir semana4
+cd semana4
+touch preguntas-semana4.txt
+touch package.json
+touch server.js
+touch reflexion-semana4.txt
+pwd
+ls
+mkdir semana5
+cd semana5
+touch preguntas-semana5.txt
+touch server.js
+touch pruebas.txt
+touch conceptos-api.txt
+touch reflexion-semana5.txt
+git add .
+git commit -m “ semana 5"
+
+git push
+git add .
+git commit -m "Actividad semana 5"
+git push
+git status
+git add .
+git commit -m "Actividad semana 5"
+git pull origin main --rebase
+git push
